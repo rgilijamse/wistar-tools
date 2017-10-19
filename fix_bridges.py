df8ec5308e28943e7d0611f24302f004ee277430
Robin Gilijamse <robin.gilijamse@ogd.nl>
2017-10-19 14:23:26 +0200

    remove hardcoded topology t1

--- fix_bridges.py
+++ fix_bridges.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 """
-Small hack to iterate over all Wistar bridges and enable LLDP/LACP (see https://github.com/Juniper/wistar/issues/12)
+Small hack to iterate over all Wistar bridges and enable LLDP/LACP
+(see https://github.com/Juniper/wistar/issues/12)
 """
 
 import subprocess
 import re
 
-BR_INFO = subprocess.Popen("ifconfig | grep t1_br", shell=True, stdout=subprocess.PIPE).stdout.read() # returns all bridges that contain t1_br
-BRIDGES = re.findall('(t1_br.*?) ', BR_INFO, re.DOTALL) # regex creates a list of only the bridge names
+# return all bridges that contain _br*
+BR_INFO = subprocess.Popen("ifconfig | grep _br", shell=True, stdout=subprocess.PIPE).stdout.read()
+# regex creates a list of only the bridge names
+BRIDGES = re.findall('(t.*_br.*?) ', BR_INFO, re.DOTALL)
 
 for bridge in BRIDGES:
-  command = "echo 65535 > /sys/class/net/" + bridge + "/bridge/group_fwd_mask"
-  subprocess.call(command, shell=True)
+    command = "echo 65535 > /sys/class/net/" + bridge + "/bridge/group_fwd_mask"
+    subprocess.call(command, shell=True)
